eaa801598a7c81ff534e771c09df6ea77f595c39
Jonathan Cheng <jcheng461@gmail.com>
2023-09-20 23:16:22 -0700

    update UI; create object using user input; method to check object

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -9,22 +9,56 @@ namespace ShapeTracker
   {
     static void Main()
     {
-      List<Triangle> allTriangles = Triangle.GetAll();
-      Triangle testTriangle = new Triangle(3, 4, 5);
-      Triangle secondTriangle = new Triangle(32, 74, 75);
-      if (allTriangles.Count == 0)
+      Console.WriteLine("Welcome to the Shape Tracker app!");
+      Console.WriteLine("Please enter an integer for side 1:");
+      string stringLength1 = Console.ReadLine();
+      Console.WriteLine("Please enter an integer for side 2:");
+      string stringLength2 = Console.ReadLine();
+      Console.WriteLine("Please enter an integer for side 3:");
+      string stringLength3 = Console.ReadLine();
+
+      int length1 = int.Parse(stringLength1);
+      int length2 = int.Parse(stringLength2);
+      int length3 = int.Parse(stringLength3);
+
+      Triangle instanceOfTriangle = new Triangle(length1, length2, length3);
+      ConfirmOrEditTriangle(instanceOfTriangle);
+    }
+
+    static void ConfirmOrEditTriangle(Triangle instance)
+    {
+      Console.WriteLine("Please confirm that your triangle's side are correct.");
+      Console.WriteLine($"Side1 has a length of {instance.GetSide1()}");
+      Console.WriteLine($"Side2 has a length of {instance.GetSide2()}");
+      Console.WriteLine($"Side3 has a length of {instance.GetSide3()}");
+      Console.WriteLine("If correct, enter 'yes'. If incorrect, enter 'no'.");
+      string userResponse = Console.ReadLine();
+
+      if (userResponse == "yes")
       {
-        Console.WriteLine("There are no triangles!");
+        CheckTriangleType(instance);
       }
       else 
       {
-        foreach (Triangle tri in allTriangles)
-        {
-          Console.WriteLine($"Side one is: {tri.GetSide1()}");
-          Console.WriteLine($"Side two is: {tri.GetSide2()}");
-          Console.WriteLine($"Side three is: {tri.GetSide3()}");
-        }
+        Console.WriteLine("Let's re-enter your 3 sides.");
+        Console.WriteLine("Please enter side1:");
+        string stringReenterSide1 = Console.ReadLine();
+        Console.WriteLine("Please enter side2:");
+        string stringReenterSide2 = Console.ReadLine();
+        Console.WriteLine("Please enter side3:");
+        string stringReenterSide3 = Console.ReadLine();
+
+        instance.SetSide1(int.Parse(stringReenterSide1));
+        instance.SetSide2(int.Parse(stringReenterSide2));
+        instance.SetSide3(int.Parse(stringReenterSide3));
+        ConfirmOrEditTriangle(instance);                
       }
+    }
+
+    static void CheckTriangleType(Triangle instance)
+    {
+      string result = instance.CheckType();
+      Console.WriteLine($"Your triangle is: {result}.");
     }
   }  
 }
