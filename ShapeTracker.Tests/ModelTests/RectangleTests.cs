84a33bfeec2fc654d7ccd438406fe23584c8182d
Jonathan Cheng <jcheng461@gmail.com>
2023-09-21 23:56:04 -0700

    set up and pass test1 for Side1 property get action

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ShapeTracker.Models;
 
@@ -9,8 +10,16 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
-      Rectangle newRectangle = new Rectangle();
+      Rectangle newRectangle = new Rectangle(3);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
+    }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1ForRectangle_Int()
+    {
+      int aSide = 5;
+      Rectangle anInstanceOfRectangle = new Rectangle(aSide);
+      Assert.AreEqual(aSide, anInstanceOfRectangle.Side1);
     }
   }
 }
