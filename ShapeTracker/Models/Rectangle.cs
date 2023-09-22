faaa356b6e3786e5a228ab761a5a9e115767140e
Jonathan Cheng <jcheng461@gmail.com>
2023-09-21 23:57:18 -0700

    update Rectangle class constructor and field; get action

--- ShapeTracker/Models/Rectangle.cs
+++ ShapeTracker/Models/Rectangle.cs
@@ -3,6 +3,16 @@ namespace ShapeTracker.Models
 {
   public class Rectangle
   {
+    private int _side1;
 
+    public int Side1 
+    {
+      get { return _side1; }
+    }
+
+    public Rectangle(int length1) 
+    {
+      _side1 = length1;
+    }
   }
 }
