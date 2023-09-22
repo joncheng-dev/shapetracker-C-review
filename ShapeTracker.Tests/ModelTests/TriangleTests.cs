cfa972e03f9d3ef62e3f30171a6d3a1e93b5244d
Jonathan Cheng <jcheng461@gmail.com>
2023-09-21 23:45:02 -0700

    set up and pass test1 for triangle constructor

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,6 +6,11 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
-    
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
+    {
+      Triangle newTriangle = new Triangle();
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+    }
   }
 }
