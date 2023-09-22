9b57013957d5e1c67545f620c84b876880b96d8f
Jonathan Cheng <jcheng461@gmail.com>
2023-09-21 23:51:34 -0700

    set up test1 for rectangle constructor and pass

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -3,5 +3,14 @@ using ShapeTracker.Models;
 
 namespace ShapeTracker.Tests
 {
-  
+  [TestClass]
+  public class RectangleTests
+  {
+    [TestMethod]
+    public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
+    {
+      Rectangle newRectangle = new Rectangle();
+      Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
+    }
+  }
 }
--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -9,7 +9,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     {
-      Triangle newTriangle = new Triangle(3);
+      Triangle newTriangle = new Triangle(3, 4, 5);
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
     }
 
@@ -17,7 +17,7 @@ namespace ShapeTracker.Tests
     public void GetSide1_ReturnsSide1_Int()
     {
       int length1 = 3;
-      Triangle newTriangle = new Triangle(length1);
+      Triangle newTriangle = new Triangle(length1, 4, 5);
       int result = newTriangle.Side1;
       Assert.AreEqual(length1, result);
     }
